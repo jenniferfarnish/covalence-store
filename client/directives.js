0a1d04b95f56bbd5410d8741270d34ee4882f7d8
Jennifer Gordy <jennifergordy@Jennifers-MacBook-Pro.local>
2017-10-23 14:03:13 -0500

    Edit Navbar

--- client/directives.js
+++ client/directives.js
@@ -1,1 +1,11 @@
 angular.module('Store.directives', [])
+
+.directive('mainNavigation', [function() {
+    return {
+        templateUrl: 'directives/navbar.html',
+        restrict: 'E',
+        scope: {
+            activePage: '='
+        }
+    };
+}]);
