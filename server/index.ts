b2cfcfdc7c1dab98c64b2f63bdacad7cfa4061ab
Clayton Ellison <claytonellison@Claytons-MacBook-Pro.local>
2017-10-23 15:50:48 -0500

    Update to Index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,7 @@
 import * as path from 'path';
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
+import stateRouting from './middleware/routing.mw';
 import api from './api';
 
 let clientPath = path.join(__dirname, '../client');
@@ -9,5 +10,7 @@ let app = express();
 app.use(express.static(clientPath));
 
 app.use(bodyParser.json());
+
+app.get('*', stateRouting);
 
 app.listen(3000);
