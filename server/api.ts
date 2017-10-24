93c248dc625bf51702bea0a2535a937fc9387806
LaPearl Hartwell <lapearlhartwell@LaPearls-MacBook-Pro.local>
2017-10-24 08:50:33 -0500

    added additional routers

--- server/api.ts
+++ server/api.ts
@@ -1,11 +1,19 @@
 import * as express from 'express';
 import products from './controllers/products.ctrl';
 import categories from './controllers/categories.ctrl';
+import misc from './controllers/misc.ctrl';
+import apparel from './controllers/apparel.ctrl';
+import purchases from './controllers/purchases.ctrl';
+import purchprod from './controllers/purchprod.ctrl';
 
 let router = express.Router();
 
 router
 .use('/products', products)
 .use('/categories', categories)
+.use('/misc', misc)
+.use('/apparel', apparel)
+.use('/purchases', purchases)
+.use('/purchprod', purchprod)
 
 export default router;
