e49d55581339de966285720b874a595852a0120b
Clayton Ellison <claytonellison@Claytons-MacBook-Pro.local>
2017-10-27 14:09:21 -0500

    Updated styling and added prerender.

--- server/index.ts
+++ server/index.ts
@@ -4,9 +4,12 @@ import * as bodyParser from 'body-parser';
 import stateRouting from './middleware/routing.mw';
 import api from './api';
 
+const prerender = require('prerender-node');
+
 let clientPath = path.join(__dirname, '../client');
 
 let app = express();
+
 app.use(express.static(clientPath));
 
 app.use(bodyParser.json());
@@ -15,4 +18,9 @@ app.use('/api', api);
 
 app.get('*', stateRouting);
 
+app.use(prerender);
+
 app.listen(process.env.PORT || 3000);
+
+prerender.set('prerenderToken', process.env.PRENDER_TOKEN);
+prerender.set('prerenderServiceUrl', 'http://localhost:1337/');
