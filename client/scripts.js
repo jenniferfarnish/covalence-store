e0bcc644ad257444ebcdc6ea42617bc5dd090114
Jennifer Gordy <jennifergordy@Jennifers-MacBook-Pro.local>
2017-10-27 21:17:28 -0500

    Removed unnecessary code

--- client/scripts.js
+++ client/scripts.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
-    let $toggleButton = $('.toggle-button');
-    let $menuWrap = $('.menu-wrap');
-
+    
     $(document).on('click', '.circle', function() {
         $('.menu-wrap').toggleClass('menu-show');
     });
